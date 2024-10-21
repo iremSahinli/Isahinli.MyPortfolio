f5781cdfb25002d769f693edb534adbde2837fe6
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-10-21 23:36:48 +0300

    Area yapıları oluşturuldu, login ekranı, yeni template, adminSeed data oluşturuldu ve entegre edildi.

---
+++ MyPortfolio/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Areas.Admin.Models;
+
+namespace MyPortfolio.Areas.Admin.Controllers
+{
+    public class AccountController : AdminBaseController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByEmailAsync(model.Email);  // Kullanıcıyı e-posta ile bul
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Geçersiz Giriş");
+                return View(model);
+            }
+
+            var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+            if (!isAdmin)
+            {
+                ModelState.AddModelError(string.Empty, "Admin değilsiniz, giriş yapamazsınız!");
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home", new { area = "Admin" });
+            }
+            else if (result.IsLockedOut)
+            {
+                ModelState.AddModelError(string.Empty, "Hesap kilitli!");
+
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Başarısız deneme, tekrar deneyiniz!");
+                return View(model);
+
+            }
+            return RedirectToAction("Index", "Home", new { area = "Admin" });
+
+        }
+    }
+}
---
+++ MyPortfolio/Seed/AdminSeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyPortfolio.Seed
+{
+    public static class AdminSeedData
+    {
+        public static async Task SeedAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string adminRole = "Admin";
+            string adminEmail = "adminirem@myportfolio.com";
+            string adminPassword = "Logintrue1991.";
+
+            if (!await roleManager.RoleExistsAsync(adminRole))  //Role varmı kontrol et yoksa yarat!
+            {
+                await roleManager.CreateAsync(new IdentityRole(adminRole));
+            }
+
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, adminRole);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (!await userManager.IsInRoleAsync(adminUser, adminRole))
+                {
+                    await userManager.AddToRoleAsync(adminUser, adminRole);
+                }
+            }
+
+
+        }
+
+    }
+}
+
+
+
