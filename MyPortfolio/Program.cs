71d000f5cbb869e7cbbbf31074b3e1132ff3e48b
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-10-23 23:25:20 +0300

    route eklendi

--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -55,4 +55,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Portfolio}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+       name: "default",
+       pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
