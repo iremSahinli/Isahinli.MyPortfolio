74ec9efb5c04628e765153d5531557e5284499dd
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2024-10-30 01:37:59 +0300

    Ziyaretçi contact ve Admin için message bölümleri aktive edildi. Kontroller gerçekleştirildi.

---
+++ MyPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Contect;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContactController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> SendMessage(Contact contact)
+        {
+
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contact.SentDate = DateTime.Now;
+                    contact.IsMessageRead = false;
+                    await _context.Contacts.AddAsync(contact);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index", "Portfolio");
+                }
+                return View("Error");
+            }
+            catch (Exception ex)
+            {
+
+                return View("Index", "Portfolio");
+            }
+
+        }
+    }
+}
---
+++ MyPortfolio/Migrations/20241029211307_dort.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    public partial class dort : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobTitle",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "SendDate",
+                table: "Contacts",
+                newName: "SentDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SentDate",
+                table: "Contacts",
+                newName: "SendDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobTitle",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
