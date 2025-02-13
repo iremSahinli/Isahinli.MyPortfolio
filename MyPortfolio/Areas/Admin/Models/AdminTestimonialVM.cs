f80aaf4c4724f0e03470fba2a09dcda18bcf55a0
İrem Şahinli <160491598+iremSahinli@users.noreply.github.com>
2025-02-13 23:12:59 +0300

    Testimonial sınıfı için ilgili Repository katmanı eklendi,  controller yapısı ve ilgili kodlar düzenlendi.

---
+++ MyPortfolio/Areas/Admin/Models/AdminTestimonialVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolio.Areas.Admin.Models
+{
+    public class AdminTestimonialVM
+    {
+        public int TestimonialId { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public DateTime SentDate { get; set; }
+        public string IsMessageRead { get; set; }
+    }
+}
